104eb69cc6e745223f06512ddd5d89f5d5f9ed01
therealjustindude <therealjustindude@gmail.com>
2020-08-25 11:31:28 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,52 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener("DOMContentLoaded", () => {
+    getTrainersData();
+})
+
+
+function getTrainersData() {
+    fetch(BASE_URL + "/trainers").then(response => {
+        return response.json()
+    }).then(trainerObj => {
+        renderTrainers(trainerObj["data"]);
+        renderPokemon(trainerObj["included"]);
+    })
+}
+
+function renderTrainers(trainerObj) {
+    trainerObj.forEach(element => {
+        createPokemonCard(element);
+    });
+}
+
+function renderPokemon(trainerObj) {
+    trainerObj.forEach(element => {
+        addPokemon(element);
+    });
+}
+
+function createPokemonCard(element) {
+    const main = document.getElementById("main-content")
+    let newDiv = document.createElement("div")
+    let newP = document.createElement("p")
+    let addBtn = document.createElement("button")
+    let newUl = document.createElement("ul")
+    let trainerName = element.attributes.name
+    addBtn.innerText = "Add Pokemon"
+    newDiv.className = "card"
+    newDiv.setAttribute("data-id", element.id)
+    newP.innerText = trainerName
+
+    main.appendChild(newDiv)
+    newDiv.appendChild(newP)
+    newDiv.appendChild(addBtn)
+    newDiv.appendChild(newUl)
+}
+
+function addPokemon(element) {
+    let trainerId = element.attributes.trainer_id.toString()
+    debugger
+}
