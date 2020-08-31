de1646f2840d39264e310df1bdc201aa82fc99de
amkera <60901491+amkera@users.noreply.github.com>
2020-08-30 17:46:40 -0700

    creating pokemon model

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,3 +1,15 @@
 class PokemonsController < ApplicationController
-  
+
+  def index
+    pokemons = Pokemon.all
+    render json: pokemons
+    #all pokemons of all trainers
+  end
+
+  def show
+    pokemon = Pokemon.find(params[:id])
+    render json: pokemon
+    #all pokemons of one trainer
+  end
+
 end
