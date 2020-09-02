084c6bd34b371bcc3d3774a7c4c23e71c2dadbbb
StevenDRiggs <sfriggs21@gmail.com>
2020-09-02 10:59:00 -0600

    WIP; committed for device change; check tests for progress

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,10 +1,33 @@
 class TrainersController < ApplicationController
   def index
-    trainers = [Trainer.all].to_h
+    trainers = Trainer.all
     pokemons = trainers.collect do |trainer|
       trainer.pokemons
     end
-    trainers.pokemons = pokemons
-    render json: MultiJson(trainers)
+    trainers = MultiJson.load(MultiJson.dump(trainers))
+    pokemons = MultiJson.load(MultiJson.dump(pokemons))
+
+    trainers.each.with_index do |trainer, i|
+      trainer.except!('created_at', 'updated_at')
+
+      pokemons[i].each do |pokemon|
+        pokemon.except!('created_at', 'updated_at')
+      end
+
+      trainer['pokemons'] = pokemons[i]
+    end
+
+    render json: MultiJson.dump(trainers)
+  end
+
+  def update
+    trainer = Trainer.find(params[:id])
+    
+    if trainer.pokemons.length < 6
+      trainer.pokemons << Pokemon.all.sample
+      trainer.save
+    end
+
+    render json: MultiJson.dump(Trainer.all)
   end
 end
--- pokemon-teams-frontend/index.js
+++ pokemon-teams-frontend/index.js
@@ -5,36 +5,65 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`
 window.addEventListener('DOMContentLoaded', () => {
     const trainers = fetch(TRAINERS_URL)
         .then(response => response.json())
-        .then(json => {console.log(JSON.stringify(json))})
-        //.then(json => {renderTrainers(json)})
-//    const pokemons = fetch(POKEMONS_URL)
-//        .then(response => response.json())
-//
-//    const main = document.querySelector('main')
-////
-//    function renderTrainers(json) {
-//        const jArray = Array.from(JSON.parse(JSON.stringify(json)).data)
-//        jArray.forEach(trainer => {
-//            //console.log(JSON.stringify(trainer))
-//            const card = document.createElement('div.card')
-//            card.textContent = JSON.stringify(trainer)
-//            card.setAttribute('data-id', trainer.id)
-//
-//            const trainerName = document.createElement('p')
-//            trainerName.textContent = trainer.attributes.name
-//            card.appendChild(trainerName)
-//
-//            const addPokemonBtn = document.createElement('button')
-//            addPokemonBtn.setAttribute('data-trainer-id', trainer.id)
-//            addPokemonBtn.textContent = 'Add Pokemon'
-//            card.appendChild(addPokemonBtn)
-//
-////            const pokemonsList = document.create('ul')
-////            trainer.pokemons.data.forEach(pokemon => {
-////                const pokemonLi = document.create('li')
-////                pokemonLi.textContent = `${pokemon.
-//
-//            main.appendChild(card)
-//        })
-//    }
+        //.then(json => {console.log(JSON.stringify(json))})
+        .then(json => {renderTrainerCards(json)})
+
 })
+
+const main = document.querySelector('main')
+
+function renderTrainerCards(json) {
+    const trainers = Array.from(json)
+
+    trainers.forEach(trainer => {
+        const card = document.createElement('div')
+        card.className = 'card'
+        card.setAttribute('data-id', trainer.id)
+
+        const name = document.createElement('p')
+        name.textContent = trainer.name
+        card.appendChild(name)
+
+        const addBtn = document.createElement('button')
+        addBtn.setAttribute('data-trainer-id', trainer.id)
+        addBtn.textContent = 'Add Pokemon'
+        addBtn.addEventListener('click', e => {requestNewPokemon(e.currentTarget.getAttribute('data-trainer-id'))})
+        card.appendChild(addBtn)
+
+        const pokeList = document.createElement('ul')
+        const pokemons = trainer.pokemons
+        pokemons.forEach(pokemon => {
+            const pokeItem = document.createElement('li')
+            pokeItem.textContent = `${pokemon.nickname} (${pokemon.species})`
+            const releaseBtn = document.createElement('button')
+            releaseBtn.className = 'release'
+            releaseBtn.setAttribute('data-pokemon-id', pokemon.id)
+            releaseBtn.textContent = 'Release'
+            pokeItem.appendChild(releaseBtn)
+            pokeList.appendChild(pokeItem)
+        })
+        card.appendChild(pokeList)
+
+
+        main.appendChild(card)
+    })
+}
+
+function requestNewPokemon(trainerId) {
+    fetch(`${TRAINERS_URL}/${trainerId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+//        body: {
+//            'trainer_id': trainerId
+//        }
+    })
+        .then(response => {response.json()})
+        .then(json => {addPokemon(json)})
+        .catch(error => {console.error(error)})
+}
+
+function addPokemon(json) {
+    console.log(JSON.stringify(json))
+}
