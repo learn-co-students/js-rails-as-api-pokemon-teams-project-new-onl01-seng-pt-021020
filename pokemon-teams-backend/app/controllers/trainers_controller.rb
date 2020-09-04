6dc023aa83ac2e9894848184b8c2fa1b22a38747
StevenDRiggs <sfriggs21@gmail.com>
2020-09-03 22:19:38 -0600

    WIP; committed for device change; check tests for progress

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,33 +1,37 @@
 class TrainersController < ApplicationController
   def index
     trainers = Trainer.all
-    pokemons = trainers.collect do |trainer|
-      trainer.pokemons
-    end
-    trainers = MultiJson.load(MultiJson.dump(trainers))
-    pokemons = MultiJson.load(MultiJson.dump(pokemons))
-
-    trainers.each.with_index do |trainer, i|
-      trainer.except!('created_at', 'updated_at')
-
-      pokemons[i].each do |pokemon|
-        pokemon.except!('created_at', 'updated_at')
-      end
-
-      trainer['pokemons'] = pokemons[i]
-    end
-
-    render json: MultiJson.dump(trainers)
+#    pokemons = trainers.collect do |trainer|
+#      trainer.pokemons
+#    end
+#    trainers = MultiJson.load(MultiJson.dump(trainers))
+#    pokemons = MultiJson.load(MultiJson.dump(pokemons))
+#
+#    trainers.each.with_index do |trainer, i|
+#      trainer.except!('created_at', 'updated_at')
+#
+#      pokemons[i].each do |pokemon|
+#        pokemon.except!('created_at', 'updated_at')
+#      end
+#
+#      trainer['pokemons'] = pokemons[i]
+#    end
+#
+    render json: trainers
   end
 
   def update
     trainer = Trainer.find(params[:id])
     
     if trainer.pokemons.length < 6
-      trainer.pokemons << Pokemon.all.sample
+      pokemon = Pokemon.all.sample
+      while trainer.pokemons.include?(pokemon) do
+        pokemon = Pokemon.all.sample
+      end
+      trainer.pokemons << pokemon
       trainer.save
     end
 
-    render json: MultiJson.dump(Trainer.all)
+    render json: trainer
   end
 end
